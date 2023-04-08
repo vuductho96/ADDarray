7c8540b378ab040e1a92e0b2acfa5deb9c0f79a5
vuductho96 <112962875+vuductho96@users.noreply.github.com>
2023-04-08 15:09:46 +0700

    update

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace AddArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   int N= Convert.ToInt32(Console.ReadLine());
+           Console.WriteLine("Nhap vao N phan tu");
+            
+            int [] songuyen  = new int[N];
+             Console.WriteLine("Nhap vao index can chen vao mang:");
+             int index=Convert.ToInt32(Console.ReadLine());
+             if (index<1 || index>=songuyen.Length){
+                Console.WriteLine("Khong thuc hien duoc");}
+else {
+ Console.WriteLine("Nhap vao so can chen:");
+ int value = Convert.ToInt32(Console.ReadLine());
+ for (int i=N-1;i>index;i--){
+    songuyen[i]=songuyen[i-1];
+
+
+}songuyen[index]=value;
+ Console.WriteLine("Mang sau khi chen la:");
+                for (int i = 0; i < N; i++)
+                {
+                    Console.Write(songuyen[i] + " ");
+                }
+             }
+            }
+        }
+    }
+
